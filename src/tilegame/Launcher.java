c7f566d8f602babc89b307b7cf09fc84600e0ad0
AlexEkdahl <alexanderekdahl@me.com>
2021-02-08 09:22:43 +0100

    First commit

---
+++ src/tilegame/Launcher.java
@@ -0,0 +1,12 @@
+package tilegame;
+
+import tilegame.display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        new Display("title!", 300,300);
+
+    }
+
+}
